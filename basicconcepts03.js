fc98ab21eb8198768bd344bd44c85291dc4e2ade
azibabc121 <azibabc121@gmail.com>
2025-08-15 02:16:41 +0500

    Fetch API

--- basicconcepts03.js
+++ basicconcepts03.js
@@ -43,3 +43,27 @@ console.log(data)
 console.log("Fetch Data")
 
 })()
+
+
+// function App() {
+//   const isLoggedIn = true;
+
+//   if (isLoggedIn) {
+//     return <h1>Welcome, Aiman!</h1>;
+//   } else {
+//     return <h1>Please log in</h1>;
+//   }
+// }
+
+
+
+
+const isDarkMode = false;
+
+console.log(isDarkMode ? "Dark Mode Enabled" : "Light Mode Enabled");
+
+const showMessage = true;
+
+showMessage && console.log("This is a conditional message!");
+
+
